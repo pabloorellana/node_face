ea9b18281f0fc8ccf0085651054f7e8252ade40b
pablo orellana <pablo.orellana.torrez@gmail.com>
2016-07-21 01:05:26 -0400

    Replaced mocked models with mongoose Added database connection Added query param options in posts

--- src/server.js
+++ src/server.js
@@ -1,26 +1,23 @@
 var express = require('express')
 var morgan = require('morgan')
 var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
 var app = express();
 
-var Users = require('./database/users.js')
-var Posts = require('./database/posts.js')
+//------- database initialization
+require('mongoose').Promise = require('q').Promise;
+mongoose.connect('mongodb://localhost:27017/mean-auth');
 
+//------- setting express middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
-/*
-{
-  id: number
-  name: string,
-  email: string,
-  birthday: date
-}
- */
+//------- importing models
+var Users = require('./database/users.js')
+var Posts = require('./database/posts.js')
 
-var users = [];
-
+//------ controllers
 app.get('/users', function (req, res) {
   Users.find().then(function (result) {
     res.send(result)
@@ -31,7 +28,7 @@ app.get('/users', function (req, res) {
 
 app.get('/users/:userId', function (req, res) {
   var userId = req.params.userId;
-  Users.findOne({id: userId}).then(function (result) {
+  Users.findOne({_id: userId}).then(function (result) {
     res.send(result)
   }).catch(function (err) {
     console.log(err)
@@ -49,7 +46,7 @@ app.post('/users', function (req, res) {
 app.put('/users/:userId', function (req, res) {
   var userId = req.params.userId;
 
-  Users.findByIdAndUpdate(userId, req.body).then(function (result) {
+  Users.findByIdAndUpdate(userId, req.body, {new: true}).then(function (result) {
     res.send(result)
   }).catch(function (err) {
     console.log(err)
@@ -69,49 +66,60 @@ app.delete('/users/:userId', function (req, res) {
 
 //--------------------------------------------------------------
 app.get('/users/:userId/posts', function (req, res) {
+  var threeDaysAgo = new Date()
+  threeDaysAgo.setDate(threeDaysAgo.getDate() - 3)
+
   var userId = req.params.userId;
-  Posts.find({userId: userId}).then(function (result) {
-    res.send(result)
-  }).catch(function (err) {
-    console.log(err)
-  })
+  var from = req.query.from || threeDaysAgo 
+  var to = req.query.to || new Date()
+  
+  Posts.find({userId: userId, createdAt: {"$gte": from, "$lt": to}})
+    .then(function (result) {
+      res.send(result)
+    }).catch(function (err) {
+      console.log(err)
+    })
 });
 
 app.get('/users/:userId/posts/:postId', function (req, res) {
   var postId = req.params.postId;
-  Posts.findOne({id: postId}).then(function (result) {
-    res.send(result)
-  }).catch(function (err) {
-    console.log(err)
-  })
+  Posts.findOne({_id: postId})
+    .then(function (result) {
+      res.send(result)
+    }).catch(function (err) {
+      console.log(err)
+    })
 });
 
 app.post('/users/:userId/posts', function (req, res) {
-  Posts.create(req.body).then(function (result) {
-    res.send(result)
-  }).catch(function (err) {
-    console.log(err)
-  })
+  Posts.create(req.body)
+    .then(function (result) {
+      res.send(result)
+    }).catch(function (err) {
+      console.log(err)
+    })
 })
 
 app.put('/users/:userId/posts/:postId', function (req, res) {
   var postId = req.params.postId;
 
-  Posts.findByIdAndUpdate(postId, req.body).then(function (result) {
-    res.send(result)
-  }).catch(function (err) {
-    console.log(err)
-  })
+  Posts.findByIdAndUpdate(postId, req.body, {new: true})
+    .then(function (result) {
+      res.send(result)
+    }).catch(function (err) {
+      console.log(err)
+    })
 })
 
 app.delete('/users/:userId/posts/:postId', function (req, res) {
   var postId = req.params.postId;
 
-  Posts.findByIdAndRemove(postId).then(function (result) {
-    res.send(result)
-  }).catch(function (err) {
-    console.log(err)
-  })
+  Posts.findByIdAndRemove(postId)
+    .then(function (result) {
+      res.send(result)
+    }).catch(function (err) {
+      console.log(err)
+    })
 })
 
 app.listen(3000, function () {
