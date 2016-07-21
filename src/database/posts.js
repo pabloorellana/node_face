8435c6767d03c4313db58a56edb1d05e35b18c16
pablo <pablo.orellana.torrez@gmail.com>
2016-07-21 15:47:33 -0400

    Added comments support

--- src/database/posts.js
+++ src/database/posts.js
@@ -4,6 +4,7 @@ var Schema = mongoose.Schema
 var PostSchema = new Schema({
     description: { type: String, required: true },
     createdAt:  { type: Date, default: Date.now },
+    comments: [{ text: String, createdAt: { type: Date, default: Date.now }}],
     userId : { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 })
 
--- src/server.js
+++ src/server.js
@@ -69,10 +69,10 @@ app.get('/users/:userId/posts', function (req, res) {
   var threeDaysAgo = new Date()
   threeDaysAgo.setDate(threeDaysAgo.getDate() - 3)
 
-  var userId = req.params.userId;
-  var from = req.query.from || threeDaysAgo 
+  var userId = req.params.userId
+  var from = req.query.from || threeDaysAgo
   var to = req.query.to || new Date()
-  
+
   Posts.find({userId: userId, createdAt: {"$gte": from, "$lt": to}})
     .then(function (result) {
       res.send(result)
@@ -82,7 +82,7 @@ app.get('/users/:userId/posts', function (req, res) {
 });
 
 app.get('/users/:userId/posts/:postId', function (req, res) {
-  var postId = req.params.postId;
+  var postId = req.params.postId
   Posts.findOne({_id: postId})
     .then(function (result) {
       res.send(result)
@@ -101,7 +101,7 @@ app.post('/users/:userId/posts', function (req, res) {
 })
 
 app.put('/users/:userId/posts/:postId', function (req, res) {
-  var postId = req.params.postId;
+  var postId = req.params.postId
 
   Posts.findByIdAndUpdate(postId, req.body, {new: true})
     .then(function (result) {
@@ -112,11 +112,58 @@ app.put('/users/:userId/posts/:postId', function (req, res) {
 })
 
 app.delete('/users/:userId/posts/:postId', function (req, res) {
-  var postId = req.params.postId;
+  var postId = req.params.postId
 
   Posts.findByIdAndRemove(postId)
     .then(function (result) {
       res.send(result)
+    }).catch(function (err) {
+      console.log(err)
+    })
+})
+
+//--------------------------------------------------------------
+app.get('/posts/:postId/comments', function (req, res) {
+  var postId = req.params.postId
+
+  Posts.findOne({_id: postId})
+    .then(function (result) {
+        res.send(result.comments)
+    }).catch(function (err) {
+      console.log(err)
+    })
+})
+
+app.post('/posts/:postId/comments', function (req, res) {
+  var postId = req.params.postId
+
+  Posts.findByIdAndUpdate(postId, {$push: {"comments": req.body } }, { new: true })
+    .then(function (result) {
+        res.send(result)
+    }).catch(function (err) {
+      console.log(err)
+    })
+})
+
+app.put('/posts/:postId/comments/:commentId', function (req, res) {
+  var postId = req.params.postId
+  var commentId = req.params.commentId
+
+  Posts.update({'comments._id': commentId }, { $set: { "comments.$.text": req.body.text } }, { new: true })
+    .then(function (result) {
+        res.send(result)
+    }).catch(function (err) {
+      console.log(err)
+    })
+})
+
+app.delete('/posts/:postId/comments/:commentId', function (req, res) {
+  var postId = req.params.postId
+  var commentId = req.params.commentId
+
+  Posts.findByIdAndUpdate(postId, {$pull: {"comments": { _id: commentId } } }, { new: true })
+    .then(function (result) {
+        res.send(result)
     }).catch(function (err) {
       console.log(err)
     })
